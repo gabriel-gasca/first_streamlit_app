68e59374e163299ecc35543976e8bc534e0e7fd7
Gabriel Gasca <109299044+gabriel-gasca@users.noreply.github.com>
2022-10-02 12:18:27 -0500

    Update streamlit_app.py
    agregaar emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 
 import streamlit
-streamlit.title('My parents new dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.title('My Mom´s New Healthy Diner')
+streamlit.header('Breakfast Favorites')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
