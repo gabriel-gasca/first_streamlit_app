dfca0d390a2a82081f7850e26014328a963310e7
Gabriel Gasca <109299044+gabriel-gasca@users.noreply.github.com>
2022-10-04 04:12:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
    if not fruit_choice:
      streamlit.error("Pease select a fruit to get informstion")
-   else  
+   else:  
      streamlit.write('The user entered ', fruit_choice)
      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
